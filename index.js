d1619b0382d0e527f26b098bc32297d4d9b4a9aa
zodwo <senanm260@gmail.com>
2023-04-23 23:13:18 +0400

    Basic Youtube Project

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const openbtn = document.querySelector(".openss");
 const sidebar = document.querySelector(".sidebar");
+const wrapper = document.querySelector(".sidebar-wrapper");
 
 openbtn.addEventListener("click", function () {
-    sidebar.classList.toggle("active")
+  sidebar.classList.toggle("active");
+  wrapper.classList.toggle("active");
+});
+
+window.addEventListener("click", function (e) {
+  if (e.target === wrapper) {
+    wrapper.classList.toggle("active");
+    sidebar.classList.toggle("active");
+  }
 });
